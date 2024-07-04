9558ddcf51d9e14137b5058871df4b7a5ec78b3b
Zephrio <76012925+CD-Chief@users.noreply.github.com>
2024-07-04 22:47:00 +0200

    Add an Add button with functionality
    no way to remove tasks yet, additionally tasks are listed with a checkbox

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const toDoList = ["Example 1", "Example 2", "Example 3"];
+const toDoList = [];
 
 const ul = document.getElementById("To-Do-List");
 
@@ -11,4 +11,23 @@ function generateList(){
     });
 }
 
-generateList();
+function addElement(){
+    let text = document.getElementById("Element-Text").value;
+    if (text){
+        //Make li element
+        let li = document.createElement("li");
+        li.textContent = text;
+
+        //Make checkbox
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+
+        //Add checbox to li
+        li.appendChild(checkbox);
+
+        //Add li to ul
+        ul.appendChild(li);
+
+        toDoList.push(text);
+    }
+}
